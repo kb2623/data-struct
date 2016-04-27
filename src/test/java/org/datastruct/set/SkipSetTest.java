493f58bc286627b17430118f8e1be3c64df76ed0
klemen <roxor1992@gmail.com>
2016-04-27 18:28:47 +0200

    Dodano okostje novega Seta. Dodani testi za testiranje Setov.

---
+++ src/test/java/org/datastruct/set/SkipSetTest.java
@@ -0,0 +1,171 @@
+package org.datastruct.set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by klemen on 4/27/16.
+ */
+public class SkipSetTest {
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	public void lower() throws Exception {
+
+	}
+
+	@Test
+	public void floor() throws Exception {
+
+	}
+
+	@Test
+	public void ceiling() throws Exception {
+
+	}
+
+	@Test
+	public void higher() throws Exception {
+
+	}
+
+	@Test
+	public void pollFirst() throws Exception {
+
+	}
+
+	@Test
+	public void pollLast() throws Exception {
+
+	}
+
+	@Test
+	public void size() throws Exception {
+
+	}
+
+	@Test
+	public void isEmpty() throws Exception {
+
+	}
+
+	@Test
+	public void contains() throws Exception {
+
+	}
+
+	@Test
+	public void iterator() throws Exception {
+
+	}
+
+	@Test
+	public void toArray() throws Exception {
+
+	}
+
+	@Test
+	public void toArray1() throws Exception {
+
+	}
+
+	@Test
+	public void add() throws Exception {
+
+	}
+
+	@Test
+	public void remove() throws Exception {
+
+	}
+
+	@Test
+	public void containsAll() throws Exception {
+
+	}
+
+	@Test
+	public void addAll() throws Exception {
+
+	}
+
+	@Test
+	public void retainAll() throws Exception {
+
+	}
+
+	@Test
+	public void removeAll() throws Exception {
+
+	}
+
+	@Test
+	public void clear() throws Exception {
+
+	}
+
+	@Test
+	public void descendingSet() throws Exception {
+
+	}
+
+	@Test
+	public void descendingIterator() throws Exception {
+
+	}
+
+	@Test
+	public void subSet() throws Exception {
+
+	}
+
+	@Test
+	public void headSet() throws Exception {
+
+	}
+
+	@Test
+	public void tailSet() throws Exception {
+
+	}
+
+	@Test
+	public void comparator() throws Exception {
+
+	}
+
+	@Test
+	public void subSet1() throws Exception {
+
+	}
+
+	@Test
+	public void headSet1() throws Exception {
+
+	}
+
+	@Test
+	public void tailSet1() throws Exception {
+
+	}
+
+	@Test
+	public void first() throws Exception {
+
+	}
+
+	@Test
+	public void last() throws Exception {
+
+	}
+
+}
