c466c4b3742da6ea172dcc681b6b0d06049cc2fb
Klemen <roxor1992@gmail.com>
2017-01-26 15:46:43 +0100

    Popravek testov za Scala programski jezik.

--- src/test/scala/org/datastruct/set/RBTreeTest.scala
+++ src/test/scala/org/datastruct/set/RBTreeTest.scala
@@ -1,22 +1,26 @@
 package org.datastruct.set
 
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
 import collection.mutable.Stack
 import org.scalatest._
 
-class StackSpec extends FlatSpec {
+@RunWith(classOf[JUnitRunner])
+class RBTreeTest extends FlatSpec {
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    assert(stack.pop() === 2)
-    assert(stack.pop() === 1)
-  }
+	"A Stack" should "pop values in last-in-first-out order" in {
+		val stack = new Stack[Int]
+		stack.push(1)
+		stack.push(2)
+		assert(stack.pop() === 2)
+		assert(stack.pop() === 1)
+	}
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[String]
-    assertThrows[NoSuchElementException] {
-      emptyStack.pop()
-    }
-  }
+	it should "throw NoSuchElementException if an empty stack is popped" in {
+		val emptyStack = new Stack[String]
+		assertThrows[NoSuchElementException] {
+			emptyStack.pop()
+		}
+	}
 }
