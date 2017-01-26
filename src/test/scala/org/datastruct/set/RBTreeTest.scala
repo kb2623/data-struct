aeacaa5a6cfd67ee4a6bade5446841c6fef8cbf1
Klemen <roxor1992@gmail.com>
2017-01-26 15:58:22 +0100

    Sedaj pravilno delujejo vsi testi.

--- src/test/scala/org/datastruct/set/RBTreeTest.scala
+++ src/test/scala/org/datastruct/set/RBTreeTest.scala
@@ -1,26 +1,22 @@
 package org.datastruct.set
 
-import org.scalatest.junit.JUnitRunner
-import org.junit.runner.RunWith
+import org.junit.{After, Before, Test}
 
-import collection.mutable.Stack
-import org.scalatest._
+class RBTreeTest {
 
-@RunWith(classOf[JUnitRunner])
-class RBTreeTest extends FlatSpec {
+	@Before
+	def setUp() {
 
-	"A Stack" should "pop values in last-in-first-out order" in {
-		val stack = new Stack[Int]
-		stack.push(1)
-		stack.push(2)
-		assert(stack.pop() === 2)
-		assert(stack.pop() === 1)
 	}
 
-	it should "throw NoSuchElementException if an empty stack is popped" in {
-		val emptyStack = new Stack[String]
-		assertThrows[NoSuchElementException] {
-			emptyStack.pop()
-		}
+	@After
+	def tearDown() {
+
 	}
+
+	@Test
+	def lower() {
+
+	}
+
 }
